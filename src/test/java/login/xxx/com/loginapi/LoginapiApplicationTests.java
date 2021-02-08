4ae4dd1fe621ebff06c3aa1ac5266aa78efe6e74
Yosuke Kagisaki <kagisaki@gmail.com>
2021-02-08 22:32:05 +0900

    20210208_初回登録

---
+++ src/test/java/login/xxx/com/loginapi/LoginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package login.xxx.com.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
